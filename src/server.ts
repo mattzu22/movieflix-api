2c432cd135f5baa5959a0ae4556c23520aa443a1
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2024-06-20 17:24:19 -0300

    rota de criação de filmes criada

--- src/server.ts
+++ src/server.ts
@@ -3,23 +3,44 @@ import { PrismaClient } from "@prisma/client";
 
 const PORT = 3000;
 const app = express();
-const prisma  = new PrismaClient();
+const prisma = new PrismaClient();
+app.use(express.json());
 
-app.get("/movies", async(req, res) =>{
- const movies  = await prisma.movie.findMany({
-    orderBy: {
-        title: "asc"
-    },
-    include: {
-        genres: true,
-        languages: true,
-    }
- });
- res.json(movies)
+app.get("/movies", async (req, res) => {
+    const movies = await prisma.movie.findMany({
+        orderBy: {
+            title: "asc"
+        },
+        include: {
+            genres: true,
+            languages: true,
+        }
+    });
+    res.json(movies)
 });
 
-app.listen(PORT, () =>{
+app.post("/movies", async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+    try {
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            }
+        });
+    } catch (error) {
+        return res.status(500).send({message: "Falha aos cadastrar um filme"})
+    }
+
+    res.status(201).send();
+});
+
+app.listen(PORT, () => {
     console.log(`Servido rodando na porta ${PORT}`);
-    
+
 })
 
