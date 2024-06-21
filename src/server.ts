53dc2eb9b4875dc13ceccea1ef0b368262139d33
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2024-06-21 14:09:43 -0300

    adicionado rota para atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -25,11 +25,11 @@ app.post("/movies", async (req, res) => {
     try {
 
         const movieWithSameTitle = await prisma.movie.findFirst({
-            where: { title: {equals: title, mode: "insensitive"} }
+            where: { title: { equals: title, mode: "insensitive" } }
         })
 
         if (movieWithSameTitle) {
-            return res.status(409).send({message: "Já existe um filmes cadastrado com esse título"})
+            return res.status(409).send({ message: "Já existe um filmes cadastrado com esse título" })
         }
 
         await prisma.movie.create({
@@ -42,11 +42,47 @@ app.post("/movies", async (req, res) => {
             }
         });
     } catch (error) {
-        return res.status(500).send({message: "Falha aos cadastrar um filme"})
+        return res.status(500).send({ message: "Falha aos cadastrar um filme" })
     }
 
     res.status(201).send();
 });
+
+app.put("/movies/:id", async (req, res) => {
+    const { id } = req.params;
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: {
+                id: Number(id)
+            }
+        });
+
+        if (!movie) {
+            return res.status(404).send({ message: "Filme não encontrado" })
+        }
+
+        await prisma.movie.update({
+            where: {
+                id: Number(id)
+            },
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            }
+        })
+
+    } catch (error) {
+        return res.status(500).send({message:"falha ao atualizar o registro do filme"})
+    }
+
+
+    return res.status(200).send()
+})
 
 app.listen(PORT, () => {
     console.log(`Servido rodando na porta ${PORT}`);
